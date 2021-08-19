0988f70d3c2e727a1db1ffb4b5e7269844fa193d
kkacan <33515415+kkacan@users.noreply.github.com>
2021-08-19 11:49:11 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    
+    UPLOAD_FOLDER = 'images/'
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+    IMAGE_PREFIX = 'http://ec2-18-185-93-51.eu-central-1.compute.amazonaws.com/images/'
